5d56400e9696bd65247fa751909b1ff702ede84c
Dominic Jones <dominic.jones@gmx.co.uk>
2018-08-09 09:20:47 +0100

    Update fmap.cpp

--- Cxx/fmap.cpp
+++ Cxx/fmap.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
 #include <utility>
 
-// code lowering of lambda 'xplusone'
-struct xplusone_t {
-  template<typename T>
-  auto operator()(T const &x) const -> decltype(x + 1) { return x+1; }
-};
 
-auto xplusone = [](auto x){ return x+1; };
-
-auto xplus = [](auto y){ return [=](auto x){ return x+y; }; };
-
-auto plus = [](auto a, auto b){ return a+b; };
-
-auto curry = [](auto f){
+auto constexpr curry = [](auto f) {
   return [=](auto x) {
     return [=](auto y) {
       return f(x,y); }; }; };
 
-auto terminal = [](auto term) {
-  return [=](auto func) {
-    return func(term); }; };
+auto constexpr terminal = [](auto x) {
+  return [=](auto f) {
+    return f(x); }; };
+
+auto constexpr fmap = [](auto f) {
+  return [=](auto x) {
+    return terminal(f(x)); }; };
+
 
 auto hello = [](auto s) { fprintf(s, "hello "); return s; };
-
-auto fmap = [](auto f) {
-  return [=](auto t) {
-    return terminal(f(t)); }; };
-
 auto world = [](auto s) { fprintf(s, "world "); return s; };
 auto newline = [](auto s) { fprintf(s, "\n"); return s; };
 
 
+auto constexpr xplusone = [](auto x){ return x+1; };
+auto constexpr xplus = [](auto y){ return [=](auto x){ return x+y; }; };
+auto constexpr plus = [](auto x, auto y){ return x+y; };
+
+
 int main()
 {
+  std::cout << plus(2,3) << std::endl;
   std::cout << xplus(2)(3) << std::endl;
 
   std::cout << curry(plus)(2)(3) << std::endl;
+  std::cout << terminal(2)(xplus)(3) << std::endl;
 
   terminal(stdout)(hello);
 
