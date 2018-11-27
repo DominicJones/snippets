840678ba02b73c8e6b43357cf4ff9583fc85054a
Dominic Jones <dominic.jones@gmx.co.uk>
2018-11-27 11:48:33 +0000

    Update identifier_traits.d

--- D/identifier_traits.d
+++ D/identifier_traits.d
@@ -1,10 +1,25 @@
+/*
+ * not helpful but considered:
+ *   typeof(name), __traits(identifier, name))
+ *   mixin template Unique(alias name)
+ */
+
+import std.stdio;
+
+struct Unique(T, string s)
+{
+    T value;
+}
+
+mixin template MakeUnique(T, string s, Args...)
+{
+    mixin("auto "~s~" = Unique!(T, s)("~Args[0].stringof~");");
+}
+
 void main()
 {
-    import std.stdio;
-    
-    int var = 123;
-    pragma(msg, typeof(var));                       // int
-    pragma(msg, typeof(__traits(identifier, var))); // string
-    writeln(var);                                   // 123
-    writeln(__traits(identifier, var));             // "var"
+    auto value = 123;
+    mixin MakeUnique!(int, "var", value);
+    pragma(msg, typeof(var));             // Unique!(int, "var")
+    writeln(var.value);                   // 123
 }
