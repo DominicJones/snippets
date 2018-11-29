c00287285842a85e12ad1b5127d4314ce8710def
Dominic Jones <dominic.jones@gmx.co.uk>
2018-11-29 15:17:39 +0000

    Create ct_address.d

---
+++ D/ct_address.d
@@ -0,0 +1,14 @@
+auto cmp(T, U)(const ref T t, const ref U u)
+{
+  return &t == &u;
+}
+
+void main()
+{
+  auto c0 = 1;
+  auto c1 = 2;
+
+  // error: "variable c0 cannot be read at compile time"
+  static assert(cmp(c0, c0));
+  static assert(!cmp(c0, c1));
+}
