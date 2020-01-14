32a79a314868e5e95a4e10b058cff66e12e0b3af
Dominic Jones <dominic.jones@gmx.co.uk>
2020-01-14 11:06:12 +0000

    Create split_string.cpp

---
+++ Cxx/split_string.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+  std::string s = "star.flow.ForceReport_11128";
+  std::string c = "_";
+  auto const split = s.find(c);
+  std::string name = s.substr(0, split);
+  std::string id = s.substr(split + 1, s.size());
+  std::cout << name << std::endl;
+  std::cout << id << std::endl;
+}
