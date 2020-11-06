4c9ae9a9f4c09b29350637138ca4e94acf041737
Dominic Jones <dominic.jones@gmx.co.uk>
2020-11-06 14:14:24 +0000

    Create reduce.cpp

---
+++ Cxx/reduce.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <array>
+
+auto reduce(int n, double * x) {
+    double y{0};
+    for (int i = 0; i != n; ++i) {
+        y += x[i];
+    }
+    return y;
+}
+
+void reduce(int n, double * x, double * y) {
+    *y = 0;
+    for (int i = 0; i != n; ++i) {
+        *y = *y + x[i];
+    }
+}
+
+void reduce_d(int n, double *x, double *xd, double *yd) {
+    *yd = 0;
+    for (int i = 0; i < n; ++i) {
+        *yd = *yd + xd[i];
+    }
+}
+
+void reduce_b(int n, double *x, double *xb, double *yb) {
+    for (int i = 0; i < n; ++i) {
+        xb[i] = xb[i] + *yb;
+    }
+    *yb = 0;
+}
+
+int main()
+{
+  std::array<double, 3> x{1,2,3};
+  double y;
+  reduce(x.size(), x.data(), &y);
+  std::cout << "reduce = " << y << std::endl;
+  std::cout << "reduce = " << reduce(x.size(), x.data()) << std::endl;
+}
