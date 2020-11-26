d410b5f4fabb69567f337e0b8c3bbbeaae31d21a
Dominic Jones <dominic.jones@gmx.co.uk>
2020-11-26 13:34:39 +0000

    Update dco_map.cpp

--- dco_map.cpp
+++ dco_map.cpp
@@ -18,11 +18,11 @@ void foo(Active const &x1, Active const &x2, Active &y)
 template<class Active>
 void foo(int n, Active const x[], Active &y)
 {
-  auto const a = sin(x[0]) * x[1];
   Active a, b(0), c, d;
 
-  MAP_FOR(Active, i, 2, n-1, 1)
-  {
+  auto const a = sin(x[0]) * x[1];
+
+  MAP_FOR(Active, i, 2, n-1, 1) {
     b += x[i];
   } MAP_FOR_END;
 
@@ -32,7 +32,7 @@ void foo(int n, Active const x[], Active &y)
     d = exp(a) * b;
   } MAP_ELSE {
     d = c * cos(b - a);
-  } MAP_IF_END ;
+  } MAP_IF_END;
 
   y = d * c * b * a;
 }
