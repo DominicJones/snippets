17e4e0922eeba0b34d081702911e4cb5d117ba17
Dominic Jones <dominic.jones@gmx.co.uk>
2020-12-17 14:44:56 +0000

    Update lambda_unique_type.cpp

--- Cxx/lambda_unique_type.cpp
+++ Cxx/lambda_unique_type.cpp
@@ -1,16 +1,26 @@
 #include <type_traits>
 
-template<typename E0, typename E1, auto UQ> struct Binary {};
+template<typename T, auto UQ = []{}>
+struct Terminal {};
 
-template<typename E0, typename E1, auto UQ = []{}> 
-auto add(E0 &&e0, E1 &&e1) -> Binary<E0, E1, UQ>
+template<typename OP, typename E0, typename E1>
+struct Binary {};
+
+struct Add;
+template<typename E0, typename E1> 
+auto add(E0 &&e0, E1 &&e1) -> Binary<Add, E0, E1>
 {
   return {};
 }
 
 int main()
 {
-  auto a = add(1, 2);
-  auto b = add(1, 2);
+  Terminal<double> a;
+  Terminal<double> b;
+  auto x = add(a, b);
+  auto y = add(a, b);
+  auto z = add(b, a);
   static_assert(!std::is_same_v<decltype(a), decltype(b)>);
+  static_assert(std::is_same_v<decltype(x), decltype(y)>);
+  static_assert(!std::is_same_v<decltype(y), decltype(z)>);
 }
