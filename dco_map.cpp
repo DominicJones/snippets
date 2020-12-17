991018e257b19caa987040f7b726dcd9bf48d441
Dominic Jones <dominic.jones@gmx.co.uk>
2020-12-17 11:14:46 +0000

    Update dco_map.cpp

--- dco_map.cpp
+++ dco_map.cpp
@@ -1,3 +1,12 @@
+/*
+  Examples of using the NAG dco/map C++ AD tool,
+  base the following paper and other sources.
+
+  http://sunsite.informatik.rwth-aachen.de/Publications/AIB/2017/2017-07.pdf
+*/
+
+// example 1
+
 template<class Active>
 void foo(Active const &x1, Active const &x2, Active &y)
 {
@@ -9,26 +18,30 @@ void foo(Active const &x1, Active const &x2, Active &y)
 int main()
 {
   usign Active = dco_map::ga1s<double>::type;
-  Active x1 = 3, x2 = 17;
+  Active x1 = 3;
+  Active x2 = 17;
   Active y1;
   dco_map::derivative(y1) = 1;
   foo(x1 , x2 , y1);
 }
 
 
+// example 2
+
 template<class Active>
 void bar(int n, Active const x[], Active &y)
 {
-  Active a, b(0), c, d;
-
   auto const a = sin(x[0]) * x[1];
 
+  Active b = 0;
   MAP_FOR(Active, i, 2, n-1, 1) {
     b += x[i];
   } MAP_FOR_END;
 
-  MAP_CALL(Active, bar(a, b, c));
+  Active c;
+  MAP_CALL(Active, baz(a, b, c));
 
+  Active d;
   MAP_IF(Active, b < c) {
     d = exp(a) * b;
   } MAP_ELSE {
