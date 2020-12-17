a85eef8324b57d8698765ad8ab7f6c53ef467323
Dominic Jones <dominic.jones@gmx.co.uk>
2020-12-17 14:34:36 +0000

    Update lambda_unique_type.cpp

--- Cxx/lambda_unique_type.cpp
+++ Cxx/lambda_unique_type.cpp
@@ -1,16 +1,16 @@
 #include <type_traits>
 
-template<typename T, T t> struct X {};
+template<typename E0, typename E1, auto UQ> struct Binary {};
 
-template<typename L , typename R, auto Unique = []{}> 
-auto f(L const &l , R const &r) -> X<decltype(Unique), Unique>
+template<typename E0, typename E1, auto UQ = []{}> 
+auto add(E0 &&e0, E1 &&e1) -> Binary<E0, E1, UQ>
 {
   return {};
 }
 
 int main()
 {
-  auto a = f(1, 2);
-  auto b = f(1, 2);
-  static_assert(!std::is_same<decltype(a), decltype(b)>::value, "");
+  auto a = add(1, 2);
+  auto b = add(1, 2);
+  static_assert(!std::is_same_v<decltype(a), decltype(b)>);
 }
