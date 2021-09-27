38ae0d724d7d046b29dd288d7ae25801ee772920
Dominic Jones <dominic.jones@gmx.co.uk>
2021-09-27 13:50:39 +0100

    Update dco_map.cpp

--- dco_map.cpp
+++ dco_map.cpp
@@ -1,5 +1,5 @@
 /*
-  Examples of using the NAG dco/map C++ AD tool,
+  Examples of using the NAG dco/map C++11 AD tool,
   base the following paper and other sources.
 
   http://sunsite.informatik.rwth-aachen.de/Publications/AIB/2017/2017-07.pdf
@@ -8,6 +8,16 @@
 
 // example 1
 
+/*
+ only one kind of terminal for input and output: the 'Active' type
+ only const and non-const qualifiers distinguish input and output
+
+ decltype(x1 * x2) = Binary<Mul, Active const &, Active const &>
+ but   decltype(a) = Binary<Mul, Active const &, Active const &> const
+
+ y is non-const and is not locally scoped
+ dco_map::derivative(y) != 0
+*/
 template<class Active>
 void foo(Active const &x1, Active const &x2, Active &y)
 {
@@ -15,12 +25,6 @@ void foo(Active const &x1, Active const &x2, Active &y)
   const auto b = sin(a) * x1;
   y = b * exp(a);
 }
-
-// decltype(x1 * x2) = Binary<Mul, Active const &, Active const &>
-// but decltype(a) = Binary<Mul, Active const &, Active const &> const
-
-// y never goes out of scope, therefore primal and adjoint
-// must be evaluated together. (Also, y.adj != 0)
 
 int main()
 {
@@ -44,26 +48,19 @@ void bar(int n, Active const x[], Active &y)
 
   Active b = 0;
   MAP_FOR(Active, i, 2, n-1, 1) {
-    const auto x2 = x[i] * x[i];
-    b += sin((i + 1) * x2);
+    const auto x2 = x[i] * x[i]; // what happens when x2 goes out of scope?
+    b += sin((i + 1) * x2); // capture x2 by value?
   } MAP_FOR_END;
 
   Active c;
   MAP_CALL(Active, baz(a, b, c)); // in: [a,b], out: [c]
 
-  Active d;
+  Active d; // dco_map::derivative(d) == 0
   MAP_IF(Active, b < c) {
-    d = exp(a) * b;
+    d = exp(a) * b; // cache as opaque expr and eval in d's dtor?
   } MAP_ELSE {
     d = c * cos(b - a);
   } MAP_IF_END;
-
-  Active e;
-  MAP_IF(Active, true) {
-    e = a * b * c;
-  } MAP_IF_END;
-
-  y = e * d;
   
-  // y = a * b * c * d;
+  y = a * b * c * d;
 }
