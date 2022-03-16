61c2cb095cba7e2ad3562945838f0d709cbde6bf
Dominic Jones <dominic.jones@gmx.co.uk>
2022-03-16 11:31:40 +0000

    Update universal_references.cpp

--- Cxx/universal_references.cpp
+++ Cxx/universal_references.cpp
@@ -1,23 +1,42 @@
-#include <iostream>
+#include <functional>
 #include <type_traits>
 
-struct Plus;
 
 template<typename OP, typename E0> struct Unary
 {
-  Unary(E0 &&e0) : e0(e0) {}
+  Unary(E0 &&e0) : e0(std::forward<E0>(e0)) {}
   E0 e0;
 };
+
+struct Plus;
 
 template<typename E0> auto plus(E0 &&e0)
 {
   return Unary<Plus, E0>(std::forward<E0>(e0));
 }
 
+
+
+template<typename T = int> struct Value
+{
+  auto rplus() const { return ::plus(*this); }
+  auto vplus() const { return ::plus(Value(*this)); }
+};
+
+
 int main()
 {
-  auto const value = 1;
-  auto p = plus(int(value));
-  static_assert(std::is_same<decltype(p.e0), int >::value, "");
-  std::cout << p.e0 << std::endl;
+  Value<> const value;
+
+  auto mr = Value<>().rplus();
+  auto mv = Value<>().vplus();
+
+  auto er = plus(value);
+  auto ev = plus(Value<>{});
+
+  static_assert(std::is_same<decltype(mr), Unary<Plus, const Value<>&>>::value, "");
+  static_assert(std::is_same<decltype(mv), Unary<Plus, Value<>>>::value, "");
+
+  static_assert(std::is_same<decltype(er), Unary<Plus, const Value<>&>>::value, "");
+  static_assert(std::is_same<decltype(ev), Unary<Plus, Value<>>>::value, "");
 }
