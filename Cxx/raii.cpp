adabc4751e6d7d300078538c6d3b9993e78e616a
Dominic Jones <dominic.jones@gmx.co.uk>
2022-05-30 16:09:43 +0100

    Update raii.cpp

--- Cxx/raii.cpp
+++ Cxx/raii.cpp
@@ -3,6 +3,16 @@
 enum class Mode { primal, adjoint };
 
 
+template<typename LC, typename LD>
+struct RAII_L
+{
+  RAII_L(LC lc, LD ld): _lc(lc), _ld(ld) { _lc(); }
+  ~RAII_L() { _ld(); }
+  LC _lc;
+  LD _ld;
+};
+
+
 template<auto f>
 struct RAII_F
 {
@@ -40,6 +50,10 @@ struct Evaluate
 
 int main()
 {
+  Evaluate const ep{Mode::primal};
+  Evaluate const ea{Mode::adjoint};
+
+  auto obj_l = RAII_L{[&](){ ep(); }, [&](){ ea(); }};
   auto obj_f = RAII_F<evaluate>{};
   auto obj_c = RAII_C<Evaluate>{};
 }
