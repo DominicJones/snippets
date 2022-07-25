a508cbde3b9baf04803291d126756371ae5a8ff4
Dominic Jones <dominic.jones@gmx.co.uk>
2022-07-25 14:04:54 +0100

    Update for_constexpr.cpp

--- Cxx/for_constexpr.cpp
+++ Cxx/for_constexpr.cpp
@@ -1,16 +1,21 @@
 // C++17, except C++20 for version_1 (but could be rewritten in C++17)
 
 
+
+
 // a simple example of evaluation of a regular function at compile time
 int constexpr pow(int base, int exp) noexcept
 {
   // if constexpr (exp == 0) { return 1; } else { return base * pow(base, exp - 1); } // error
-  return (exp == 0)? (1): (base * pow(base, exp - 1)); // ok
+  if (exp == 0) { return 1; } else { return base * pow(base, exp - 1); } // ok
+  // return (exp == 0)? (1): (base * pow(base, exp - 1)); // ok
 }
 
 static_assert(pow(3, 2) == 9);
 static_assert(pow(4, 2) == 16);
 static_assert(pow(5, 2) == 25);
+
+
 
 
 // c-t factorial using template syntax
@@ -55,6 +60,22 @@ namespace version_2
   {
     auto result = 1;
     for_constexpr(0, end, 1, [&result](int it){ result *= (it+1); });
+    return result;
+  }
+
+  static_assert(factorial(3) == 6);
+  static_assert(factorial(4) == 24);
+  static_assert(factorial(5) == 120);
+}
+
+
+// c-t factorial using a regular for loop
+namespace version_3
+{
+  auto constexpr factorial(int end) noexcept
+  {
+    auto result = 1;
+    for (int it = 0; it != end; ++it) result *= (it+1);
     return result;
   }
 
