79edc12a1b89ca23f6f8b1da7a593458df7f1dc4
Dominic Jones <dominic.jones@gmx.co.uk>
2022-12-08 11:53:31 +0000

    Update template_template_lambda.cpp

--- Cxx/template_template_lambda.cpp
+++ Cxx/template_template_lambda.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-template<typename T>
-auto constexpr c_cast = [](auto v){ return (T)v; };
+enum class Cast { Static, Dynamic, Reinterpret, C_Style, };
+
+template<Cast c, typename T>
+auto constexpr cast = [](auto &&v)
+{
+  if constexpr (c == Cast::Static) return static_cast<T>(v);
+  if constexpr (c == Cast::Dynamic) return dynamic_cast<T>(v);
+  if constexpr (c == Cast::Reinterpret) return reinterpret_cast<T>(v);
+  /* (c == Cast::C_Style) */ return (T)v;
+};
 
 int main() {
-  std::cout << c_cast<int>(3.14) << std::endl;
+  std::cout << cast<Cast::Static, int>(3.14) << std::endl;
 }
