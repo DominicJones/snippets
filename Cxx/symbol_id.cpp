374ae4abdad3def6644f96730bfc11ef2cc86d12
Dominic Jones <dominic.jones@gmx.co.uk>
2024-08-27 17:28:58 +0100

    Update symbol_id.cpp

--- Cxx/symbol_id.cpp
+++ Cxx/symbol_id.cpp
@@ -4,3 +4,22 @@ struct symbol_id
   static auto constexpr singleton = []{};
   static auto constexpr void = std::addressof(singleton);
 };
+
+template<auto ID = symbol_id<decltype([]{})>{}>
+struct symbol
+{
+  static auto constexpr id = ID;
+};
+
+template<typename Lhs, typename Rhs>
+bool constexpr operator<(symbol_id<Lhs>, symbol<Rhs>)
+{
+//  return symbol_id<Lhs>::address < symbol_id<Rhs>::address;
+  return std::less{}(symbol_id<Lhs>::address, symbol_id<Rhs>::address);
+}
+
+template<typename Lhs, typename Rhs>
+std::strong_ordering constexpr operator<=>(symbol_id<Lhs>, symbol<Rhs>)
+{
+  return std::compare_three_way{}(symbol_id<Lhs>::address, symbol_id<Rhs>::address);
+}
