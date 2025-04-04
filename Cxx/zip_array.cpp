5aefe5bc5ec20fafc2647d12921fca1936fd2c83
Dominic Jones <dominic.jones@gmx.co.uk>
2025-04-04 11:35:27 +0100

    Update zip_array.cpp

--- Cxx/zip_array.cpp
+++ Cxx/zip_array.cpp
@@ -12,35 +12,33 @@ struct Array : std::array<int, sizeof...(I)>
 };
 
 
-template<typename ...V>
+template<typename V0, typename ...V>
 struct Zip
 {
   static consteval auto eval()
   {
-    auto constexpr size{sizeof...(V)};
-
-    auto constexpr zip = [](auto ...v) {
-      auto result = std::array<int, size>{};
+    auto constexpr zip = [](auto v0, auto ...v) {
+      auto result = std::array<int, v0.size()>{};
       result.fill(1);
-      for (std::size_t i = 0; i != size; ++i) {
-        result[i] *= (v[i] * ...);
+      for (std::size_t i = 0; i != v0.size(); ++i) {
+        result[i] *= v0[i] * (v[i] * ...);
       }
       return result;
-    }(V{}...);
+    }(V0{}, V{}...);
     
     return [&]<auto ...I>(std::index_sequence<I...>) {
       return Array<zip[I]...>{};
-    }(std::make_index_sequence<size>{});
+    }(std::make_index_sequence<zip.size()>{});
   }
 
   using type = decltype(eval());
 };
 
 
-using Test = Zip<Array<1,2,3>, 
-                 Array<2,3,4>, 
-                 Array<3,4,5>>::type;
-using Expected = Array<6,24,60>;
+using Test = Zip<Array<1,1,2,3>, 
+                 Array<1,2,3,4>, 
+                 Array<1,3,4,5>>::type;
+using Expected = Array<1,6,24,60>;
 
 static_assert(std::same_as<Test, Expected>);
 
