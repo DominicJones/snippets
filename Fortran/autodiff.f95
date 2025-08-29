d3b8c0e430d45e9663c659b599c240085523b399
Dominic Jones <dominic.jones@gmx.co.uk>
2025-08-29 11:31:30 +0100

    Update autodiff.f95

--- Fortran/autodiff.f95
+++ Fortran/autodiff.f95
@@ -1,8 +1,11 @@
 ! http://www-tapenade.inria.fr:8080/tapenade/
 
-SUBROUTINE EVALUATE(x, f)
+SUBROUTINE HYPOT(a, b, r)
   IMPLICIT NONE
-  REAL :: f
-  REAL :: x
-  f = x*x
-END SUBROUTINE EVALUATE
+  REAL :: a, b, r, t0, t1, t2
+  INTRINSIC SQRT
+  t0 = a*a
+  t1 = b*b
+  t2 = SQRT(t0 + t1)
+  r = t2
+END SUBROUTINE
